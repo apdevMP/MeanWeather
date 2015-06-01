fbdbd95cae6e7a427f14d3faf499d41dfbaf9119
Vanessa Perciballi <v.perciballi@gmail.com>
2015-06-01 12:02:29 +0200

    first commit

---
+++ WeatherMean/src/it/apdev/weathermean/presentation/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package it.apdev.weathermean.presentation;
+
+import android.app.Activity;
+
+/**
+ * @author Vanessa
+ *
+ */
+public class SplashScreenActivity extends Activity
+{
+
+}
