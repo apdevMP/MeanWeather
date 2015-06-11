e96cc7e04b78d1608465fc06671a852af1bb998a
Vanessa Perciballi <v.perciballi@gmail.com>
2015-06-11 18:34:08 +0200

    inserito location manager nello splashscreen per il recupero della posizione corrente; creato database delle città e cursor adapter relativo. Inserita la possibilità di eliminare un record dal DB tramite imageButton sul relativo item della lista.

---
+++ WeatherMean/src/it/apdev/weathermean/storage/DBHelper.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package it.apdev.weathermean.storage;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * @author Vanessa
+ *
+ */
+public class DBHelper extends SQLiteOpenHelper
+{
+
+	public static final String	DBNAME			= "CITIES_DB";
+
+
+	public DBHelper(Context context, String name, CursorFactory factory, int version)
+	{
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite
+	 * .SQLiteDatabase)
+	 */
+	@Override
+	public void onCreate(SQLiteDatabase db)
+	{
+		// TODO Auto-generated method stub
+
+		String q = "CREATE TABLE " + DBStrings.TBL_NAME + " ( _id INTEGER PRIMARY KEY AUTOINCREMENT," + DBStrings.FIELD_CITY + " TEXT," + DBStrings.FIELD_COUNTRY + " TEXT)";
+		db.execSQL(q);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite
+	 * .SQLiteDatabase, int, int)
+	 */
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ WeatherMean/src/it/apdev/weathermean/storage/DBStrings.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package it.apdev.weathermean.storage;
+
+/**
+ * @author Vanessa
+ *
+ */
+public class DBStrings
+{
+	public static final String	FIELD_ID		= "_id";
+	public static final String	FIELD_CITY		= "cityName";
+	public static final String	FIELD_COUNTRY	= "countryCode";
+	public static final String	TBL_NAME		= "cities";
+}
